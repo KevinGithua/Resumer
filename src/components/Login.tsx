a3c1e3f36feecf7efda6b56d2fa87b08818ab188
KevinGithua <113514699+KevinGithua@users.noreply.github.com>
2024-10-01 08:40:15 +0300

    fixed the special character issue in login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -138,7 +138,7 @@ const LogInComponent = () => {
             onClick={() => router.push("/signup")}
             className="text-sm text-blue-500 hover:underline"
           >
-            Don't have an account? Sign Up
+            Don&apos;t have an account? Sign Up
           </button>
         </div>
       </form>
